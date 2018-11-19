65b09c60d3f5ecca08562aff7348212f94a28ee7
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-11-19 16:40:22 +0100

    added IdProvder.get_class_name() method

--- idprovider/models.py
+++ idprovider/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class IdProvider(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def get_class_name(self):
+        class_name = self._meta.model_name
+        return class_name
