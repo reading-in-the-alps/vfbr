babeeefc21329a53579693877e9a6eb6d91661c1
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-11-19 12:36:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Verfachbuchregister-App
+
+A [djangobaseproject](https://github.com/acdh-oeaw/djangobaseproject) based web application to publish summaries of Verfachbücher from
+
+* Stadtgericht Bruneck
+* Landgericht St. Michaelsburg
+* a third one to come
+
+for the years 1750-1800 created by Michael Prokosch and Michael Span in the context of the FWF-funded project Reading in the Alps.
---
+++ webpage/metadata.py
@@ -0,0 +1,24 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'RITA-VFBR',
+    'author': 'Peter Andorfer, Michael Span, Michael Prokosch',
+    'subtitle': 'Register der Verfachbücher St. Michaelsburg und Bruneck 1750-1800',
+    'description': 'Im Rahmen des Projektes Reading in the Alps wurden die Verfachbücher der\
+    Gerichte St. Michaelsburg und Bruneck für den Zeitraum 1750 bis 1800 systematisch und\
+    vollständig von Michael Span und Michael Prokosch durchgesehen und die darin enthaltenen\
+    Einträge in Registerform erfasst. Die vorliegende Web Applikation stellt diesen, primär als\
+    internen Forschungsbehelf konzipierten Datenbestand der öffentlichkeit zur Verfügung.',
+    'github': 'https://github.com/reading-in-the-alps/vfbr',
+    'purpose_de': 'Ziel von RITA-VFBR ist die Veröffentlichung von Forschungsdaten.',
+    'purpose_en': 'The purpose of RITA-VFBR is the publication of research data.',
+    'version': '0.0.1',
+    'matomo_id': 'provide some',
+    'matomo_url': '//matomo.acdh.oeaw.ac.at/',
+    'imprint': '/imprint',
+    'social_media': [
+        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
+        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
+    ]
+}
